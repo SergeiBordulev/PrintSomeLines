df0c8b71c65853482afc180148f0a1ae223e734b
SergeyBordulev <bordulev@yahoo.com>
2021-02-10 21:33:24 +0700

    Initial commit

---
+++ src/PrintSomeLines.java
@@ -0,0 +1,13 @@
+public class PrintSomeLines {
+    public static void main(String[] args) {
+        System.out.println("First line:" + System.lineSeparator() + System.lineSeparator() + System.lineSeparator() + "End line");
+
+        System.out.println("\"30\" августа 2014г.");
+        System.out.println("C:\\Windows\\System32\\Drivers\\etc\\hosts");
+
+        int a = 10;
+        int b = 14;
+        int result = a + b;
+        System.out.println("Результат: " + result);
+    }
+}
